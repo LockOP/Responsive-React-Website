f04b3e5802f3a1850231bb8003117a2cbb820052
LockOP <madhavaarul@gmail.com>
2023-05-30 12:18:35 +0530

    e2

--- src/App.js
+++ src/App.js
@@ -6,19 +6,15 @@ import Contact from "./Components/Pages/Contact";
 
 import Menu from "./Components/NavBar/menu";
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
 
 
 function App() {
-  const [c,uc] = useState(0)
    return (
     <>
       <Menu/>
       <div className="route-container">
         {/* <AnimatedCursor color="0,240,255" outerAlpha={1} outerSize={"15px"} innerSize={"30px"} /> */}
         <Routes >
-          {c<1? <div><Home/></div>: <div/>}
-          {uc(1)}
           <Route path="/" element={<div><Home/></div>}/>
           <Route path="/services" element={<Services/>}/>
           <Route path="/work" element={<Work/>}/>
